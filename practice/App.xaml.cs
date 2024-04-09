fed956e352aabe4b32a6aed134c34928c88fc266
combatsasality <m3sckt@gmail.com>
2024-04-09 15:40:42 +0300

    WIP local

--- practice/App.xaml.cs
+++ practice/App.xaml.cs
@@ -1,17 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
+using System.Collections.ObjectModel;
 using System.Data;
+using System.Globalization;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
+using System.Collections.Generic;
+using practice.Properties;
+
 
 namespace practice
 {
-    /// <summary>
-    /// Логика взаимодействия для App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private static List<CultureInfo> _availableLanguages = new List<CultureInfo>();
+        public static ReadOnlyCollection<CultureInfo> availableLanguages
+        {
+            get
+            {
+                return _availableLanguages.AsReadOnly();
+            }
+        }
+        public App()
+        {
+            _availableLanguages.Clear();
+            _availableLanguages.Add(new CultureInfo("en-US"));
+            _availableLanguages.Add(new CultureInfo("uk-UA"));
+
+            //Language = Settings.Default.Language;
+        }
+        public static CultureInfo Language
+        {
+            get
+            {
+                return System.Threading.Thread.CurrentThread.CurrentUICulture;
+            }
+            set
+            {
+                if (value==null) throw new ArgumentNullException("value");
+                if (value.Equals(System.Threading.Thread.CurrentThread.CurrentUICulture)) return;
+
+                System.Threading.Thread.CurrentThread.CurrentUICulture = value;
+                System.Threading.Thread.CurrentThread.CurrentCulture = value;
+
+                ResourceDictionary dict = new ResourceDictionary();
+                dict.Source = new Uri(String.Format("Resources/lang/{0}.xaml", value.Name), UriKind.Relative);
+                //Application.Current.Resources.MergedDictionaries.Add(dict);
+                ResourceDictionary oldDict = (
+                    from d in Application.Current.Resources.MergedDictionaries
+                    where d.Source != null && d.Source.OriginalString.StartsWith("Resources/lang/")
+                    select d).FirstOrDefault();
+                if (oldDict != null)
+                {
+                    int index = Application.Current.Resources.MergedDictionaries.IndexOf(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Remove(oldDict);
+                    Application.Current.Resources.MergedDictionaries.Insert(index, dict);
+                }
+                else
+                {
+                    Application.Current.Resources.MergedDictionaries.Add(dict);
+                }
+                Settings.Default.Language = value;
+                Settings.Default.Save();
+            }
+        }
     }
 }
